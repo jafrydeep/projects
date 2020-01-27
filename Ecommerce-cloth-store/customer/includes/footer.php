6c1ce800f501776168e712c28de30e19e6d37f73
Jafry Mondol Deep <60100543+jafrydeep@users.noreply.github.com>
2020-01-27 13:58:46 +0600

    Add files via upload

---
+++ Ecommerce-cloth-store/customer/includes/footer.php
@@ -0,0 +1,19 @@
+
+<footer>
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; 2020 Noman's Fashion&trade;
+			</div>
+
+			<div class="developer">
+				Developed by <a href="/developer.php"><b>TEAM</b></a>
+			</div>
+
+			<!-- <div class="designby">
+				Design by Abdullah Al Noman
+			</div> -->
+		</div>
+	</div>
+</footer>
---
+++ Ecommerce-cloth-store/includes/footer.php
@@ -0,0 +1,19 @@
+
+<footer>
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; 2020 Noman's Fashion&trade;
+			</div>
+
+			<div class="developer">
+				Developed by <a href="http://jafrydeep.com"><b>JAFRY MONDOL DEEP</b></a>
+			</div>
+
+			<!-- <div class="designby">
+				Design by Abdullah Al Noman
+			</div> -->
+		</div>
+	</div>
+</footer>
